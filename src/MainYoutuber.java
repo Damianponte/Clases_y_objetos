48f33d28665174cf1adcf3c102ef76148494c44c
dami <damianponte@hotmail.es>
2026-01-13 17:09:22 +0100

    empezando parte del main

--- src/MainYoutuber.java
+++ src/MainYoutuber.java
@@ -1,4 +1,8 @@
 public class MainYoutuber {
     //Crea una clase llamada Youtuber con los atributos nombre del canal, categoría y número de suscriptores.
     //Desde la clase principal, crea tres objetos Youtuber, setéalos e imprime y actualiza el número de suscriptores de uno de ellos.
+
+
 }
+
+
