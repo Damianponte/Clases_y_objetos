4f2b4faf1d3368875791a8c9f63599fb62323212
dami <damianponte@hotmail.es>
2026-01-14 19:38:21 +0100

    parte main del ejercicio manga completada

--- src/MainManga.java
+++ src/MainManga.java
@@ -1,2 +1,21 @@
 public class MainManga {
+    public static void main(String[] args) {
+
+        //Crea una clase Manga con los atributos título, autor, número de volumen y un boolean que indique si está leído o no.
+        //Desde el main, crea un manga, muestra su información y márcalo como leído.
+
+        Manga manga1 = new Manga();
+
+        manga1.setTitulo("One Piece");
+        manga1.setAutor("Echiro Oda");
+        manga1.setVolumen(1);
+        manga1.setLeido(true);
+
+        System.out.println("");
+        System.out.println(manga1.getTitulo() + " es la obra más conocida de " + manga1.getAutor());
+        System.out.println(
+                "Volumen: " + manga1.getVolumen() +" ¿ ha sido Leído?: " + manga1.isLeido()
+
+        );
+    }
 }
