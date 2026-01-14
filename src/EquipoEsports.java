22f1590749c7e626085e14688adee2fa0ae07738
dami <damianponte@hotmail.es>
2026-01-14 19:44:48 +0100

    empezando los getters and setters de la clase EquipoEsports.

---
+++ src/EquipoEsports.java
@@ -0,0 +1,19 @@
+public class EquipoEsports {
+
+   // Crea una clase EquipoEsports con los atributos nombre, juego, país y ranking mundial.
+
+   private String nombre;
+   private String juego;
+   private String pais;
+   private int ranking;
+
+    public String getNombre(){
+        return nombre.toUpperCase();
+    }
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+
+    }
+
+
+}
